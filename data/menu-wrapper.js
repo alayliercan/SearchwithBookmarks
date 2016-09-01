d9d51b17e3be9e324393e5b7b4d58fc141dc6bfc
Ercan <ercanalayli@yandex.com>
2016-09-01 03:50:31 +0100

    linting - happiness

--- data/menu-wrapper.js
+++ data/menu-wrapper.js
@@ -1,158 +1,160 @@
 const ss = require('sdk/simple-storage');
 const { Class } = require('sdk/core/heritage');
-const { defer } = require("sdk/core/promise");
+const { defer } = require('sdk/core/promise');
 const { search } = require('sdk/places/bookmarks');
-const { sha256 } = require("./sha256.js");
-const { firefoxIcons } = require("./firefox-icons.js");
+const { sha256 } = require('./sha256.js');
+const { firefoxIcons } = require('./firefox-icons.js');
 
-//Parse given URL
-function extractDomain(url) {
-    var objUrl = require("sdk/url").URL(url);
-    var domain = objUrl.scheme + "://" + objUrl.host;
-    return domain;
+// Parse given URL
+function extractDomain (url) {
+	var objUrl = require('sdk/url').URL(url);
+	var domain = objUrl.scheme + '://' + objUrl.host;
+	return domain;
 }
 
-//Search user's bookmarks for specified keyword
-function searchBookmarks(keyword) {
-    var deferred = defer();
-    try {
-        search({
-                query: keyword
-            }, {
-                sort: 'title'
-            })
-            .on('end', function(foundBookmarks) {
-                deferred.resolve(foundBookmarks);
-            });
-    } catch (e) {
-        //console.log(e.message);
-        deferred.reject(e);
-    } finally {
-        return deferred.promise;
-    }
+// Search user's bookmarks for specified keyword
+function searchBookmarks (keyword) {
+	var deferred = defer();
+	try {
+		search({
+			query: keyword
+		}, {
+			sort: 'title'
+		})
+		.on('end', function (foundBookmarks) {
+			deferred.resolve(foundBookmarks);
+		});
+	} catch (e) {
+		// console.error(e.message);
+		deferred.reject(e);
+	} finally {
+		return deferred.promise;
+	}
 }
 
 var SWBMenuItem = Class({
-    initialize: function initialize(title, url, tags, orphan, favicon, order) {
-        this.identity = sha256.convert(url);
-        this.domain = extractDomain(url);
-        this.title = title;
-        this.url = url;
-        this.tags = typeof tags !== 'undefined' ? tags : [];
-        this.orphan = typeof orphan !== 'undefined' ? orphan : false;
-        this.favicon = typeof favicon !== 'undefined' ? favicon : firefoxIcons.BOOKMARKDEFAULT;
-        this.order = typeof order !== 'undefined' ? order : 0;
-    },
-    type: 'SWBMenuItem',
-    toString: function toString() {
-        return "{ 'identity' : '" + this.identity + "','name':'" + this.title + "'}";
-    }
+	initialize: function initialize (title, url, tags, orphan, favicon, order) {
+		this.identity = sha256.convert(url);
+		this.domain = extractDomain(url);
+		this.title = title;
+		this.url = url;
+		this.tags = typeof tags !== 'undefined' ? tags : [];
+		this.orphan = typeof orphan !== 'undefined' ? orphan : false;
+		this.favicon = typeof favicon !== 'undefined' ? favicon : firefoxIcons.BOOKMARKDEFAULT;
+		this.order = typeof order !== 'undefined' ? order : 0;
+	},
+	type: 'SWBMenuItem',
+	toString: function toString () {
+		return '{ \'identity\' : ' + this.identity + '\', \'name\': \'' + this.title + '}';
+	}
 });
 
 var SWBMenu = Class({
-    initialize: function initialize(name) {
-        this.name = name;
-        this.menu = {
-            'items': [],
-            'count': 0
-        };
-        if (ss.storage['swbmenu'] != null) {
-            this.menu = JSON.parse(ss.storage['swbmenu']);
-        };
-    },
-    type: 'SWBMenu',
-    updateByBookmarks: function updateByBookmarks(searchKeyword, callback) {
-        var deferredSearchBookmarks = searchBookmarks(searchKeyword);
-        var thisRef = this;
-        deferredSearchBookmarks.then(function(foundBookmarks) {
-            if (thisRef.menu.count == 0) {
-                for (var i = 0; i < foundBookmarks.length; i++) {
-                    var swbMenuItem = new SWBMenuItem(foundBookmarks[i].title, foundBookmarks[i].url, foundBookmarks[i].tags, false);
-                    thisRef.insertMenuItem(swbMenuItem);
-                }
-            }
-            if (thisRef.menu.count > 0) {
-                for (var j = 0; j < thisRef.menu.items.length; j++) {
-                    thisRef.menu.items[j].orphan = true;
-                }
-                for (var k = 0; k < foundBookmarks.length; k++) {
-                    var swbMenuItem = new SWBMenuItem(foundBookmarks[k].title, foundBookmarks[k].url, foundBookmarks[k].tags, false);
-                    thisRef.updateMenuItem(swbMenuItem);
-                }
-            }
-            thisRef.save(callback);
-        }, function(e) {
-            //console.log(e.message);
-        });
-    },
-    insertMenuItem: function insertMenuItem(menuItem) {
-        if (menuItem instanceof SWBMenuItem) {
-            this.menu.items.push(menuItem);
-            this.menu.count++;
-        }
-    },
-    updateMenuItem: function updateMenuItem(menuItem) {
-        if (menuItem instanceof SWBMenuItem) {
-            var idx = this.menu.items.indexOf(menuItem.identity);
-            var idx = this.menu.items.map((el) => el.identity).indexOf(menuItem.identity);
-            if (idx !== -1) {
-                this.menu.items[idx].title = menuItem.title;
-                this.menu.items[idx].url = menuItem.url;
-                this.menu.items[idx].tags = menuItem.tags;
-                this.menu.items[idx].orphan = menuItem.orphan;
-            } else {
-                this.insertMenuItem(menuItem);
-            }
-        }
-    },
-    updateMenuItemFavIcon: function updateMenuItemFavIcon(menuItem) {
-        if (menuItem instanceof SWBMenuItem) {
-            var itemsWithSameDomain = this.menu.items.filter(x => x.domain === menuItem.domain);
-            for (var l = 0; l < itemsWithSameDomain.length; l++) {
-                var idx = this.menu.items.map((el) => el.identity).indexOf(itemsWithSameDomain[l].identity);
-                if (idx !== -1) {
-                    this.menu.items[idx].favicon = menuItem.favicon;
-                }
-            }
-        }
-        this.save();
-    },
-    updateMenuItemOrder: function updateMenuItemFavIcon(menuItemsOrdered, callback) {
-        for (let i = 0; i < menuItemsOrdered.length; i++) {
-            let splittedItem = menuItemsOrdered[i].split('_');
-            let itemId = splittedItem[0];
-            var idx = this.menu.items.map((el) => el.identity).indexOf(itemId);
-            if (idx !== -1) {
-                this.menu.items[idx].order = i;
-            }
-        }
-        this.save(callback);
-    },
-    findById: function findById(menuId) {
-        var idx = this.menu.items.map((el) => el.identity).indexOf(menuId);
-        if (idx !== -1) {
-            return this.menu.items[idx];
-        }
-        return null;
-    },
-    sortItems: function sortItems(a, b) {
-        if (a.order < b.order)
-            return -1;
-        if (a.order > b.order)
-            return 1;
-        return 0;
-    },
-    save: function save(callback) {
-        //remove orphan entries and save
-        this.menu.items = this.menu.items.filter(x => x.orphan === false);
-        this.menu.items = this.menu.items.sort(this.sortItems);
-        this.menu.count = this.menu.items.length;
-        ss.storage['swbmenu'] = JSON.stringify(this.menu);
-        if (callback)
-            callback();
-    }
+	initialize: function initialize (name) {
+		this.name = name;
+		this.menu = {
+			'items': [],
+			'count': 0
+		};
+		if (ss.storage['swbmenu'] != null) {
+			this.menu = JSON.parse(ss.storage['swbmenu']);
+		}
+	},
+	type: 'SWBMenu',
+	updateByBookmarks: function updateByBookmarks (searchKeyword, callback) {
+		var deferredSearchBookmarks = searchBookmarks(searchKeyword);
+		var thisRef = this;
+		deferredSearchBookmarks.then(function (foundBookmarks) {
+			if (thisRef.menu.count === 0) {
+				for (let i = 0; i < foundBookmarks.length; i++) {
+					let swbMenuItem = new SWBMenuItem(foundBookmarks[i].title, foundBookmarks[i].url, foundBookmarks[i].tags, false);
+					thisRef.insertMenuItem(swbMenuItem);
+				}
+			}
+			if (thisRef.menu.count > 0) {
+				for (let j = 0; j < thisRef.menu.items.length; j++) {
+					thisRef.menu.items[j].orphan = true;
+				}
+				for (let k = 0; k < foundBookmarks.length; k++) {
+					let swbMenuItem = new SWBMenuItem(foundBookmarks[k].title, foundBookmarks[k].url, foundBookmarks[k].tags, false);
+					thisRef.updateMenuItem(swbMenuItem);
+				}
+			}
+			thisRef.save(callback);
+		}, function (e) {
+			// console.error(e.message);
+		});
+	},
+	insertMenuItem: function insertMenuItem (menuItem) {
+		if (menuItem instanceof SWBMenuItem) {
+			this.menu.items.push(menuItem);
+			this.menu.count++;
+		}
+	},
+	updateMenuItem: function updateMenuItem (menuItem) {
+		if (menuItem instanceof SWBMenuItem) {
+			var idx = this.menu.items.indexOf(menuItem.identity);
+			idx = this.menu.items.map((el) => el.identity).indexOf(menuItem.identity);
+			if (idx !== -1) {
+				this.menu.items[idx].title = menuItem.title;
+				this.menu.items[idx].url = menuItem.url;
+				this.menu.items[idx].tags = menuItem.tags;
+				this.menu.items[idx].orphan = menuItem.orphan;
+			} else {
+				this.insertMenuItem(menuItem);
+			}
+		}
+	},
+	updateMenuItemFavIcon: function updateMenuItemFavIcon (menuItem) {
+		if (menuItem instanceof SWBMenuItem) {
+			var itemsWithSameDomain = this.menu.items.filter((x) => x.domain === menuItem.domain);
+			for (var l = 0; l < itemsWithSameDomain.length; l++) {
+				var idx = this.menu.items.map((el) => el.identity).indexOf(itemsWithSameDomain[l].identity);
+				if (idx !== -1) {
+					this.menu.items[idx].favicon = menuItem.favicon;
+				}
+			}
+		}
+		this.save();
+	},
+	updateMenuItemOrder: function updateMenuItemFavIcon (menuItemsOrdered, callback) {
+		for (let i = 0; i < menuItemsOrdered.length; i++) {
+			let splittedItem = menuItemsOrdered[i].split('_');
+			let itemId = splittedItem[0];
+			var idx = this.menu.items.map((el) => el.identity).indexOf(itemId);
+			if (idx !== -1) {
+				this.menu.items[idx].order = i;
+			}
+		}
+		this.save(callback);
+	},
+	findById: function findById (menuId) {
+		var idx = this.menu.items.map((el) => el.identity).indexOf(menuId);
+		if (idx !== -1) {
+			return this.menu.items[idx];
+		}
+		return null;
+	},
+	sortItems: function sortItems (a, b) {
+		if (a.order < b.order) {
+			return -1;
+		}
+		if (a.order > b.order) {
+			return 1;
+		}
+		return 0;
+	},
+	save: function save (callback) {
+		// remove orphan entries and save
+		this.menu.items = this.menu.items.filter((x) => x.orphan === false);
+		this.menu.items = this.menu.items.sort(this.sortItems);
+		this.menu.count = this.menu.items.length;
+		ss.storage['swbmenu'] = JSON.stringify(this.menu);
+		if (callback) {
+			callback();
+		}
+	}
 });
-
 
 exports.SWBMenu = SWBMenu;
 exports.SWBMenuItem = SWBMenuItem;
--- data/settings.js
+++ data/settings.js
@@ -1,68 +1,71 @@
-/// <reference path="../../typings/jquery/jquery.d.ts"/>
-/// <reference path="../../typings/showdown/showdown.d.ts"/>
+// <reference path="../../typings/jquery/jquery.d.ts"/>
+// <reference path="../../typings/showdown/showdown.d.ts"/>
+/* global $,self,location,showdown */
 
 $(document).ready(function () {
-    //Send message to page from add-on
-    self.port.on("currentmenuitems", function (menuItems) {
-        var bookmarkItems = '';
-        if (menuItems.length == 0) {
-            bookmarkItems = '<li id="nobookmarkfound" class="bookmark-menu-item"><img class="bookmark-menu-item-image" src="chrome://mozapps/skin/places/defaultFavicon.png"/>No searchable bookmark found!</li>';
-        } else {
-            $.each(menuItems, function (index, value) {
-                var template = '<li id="{identity}_{index}" class="bookmark-menu-item"><img class="bookmark-menu-item-image" src="{favicon}"/>{title}</li>';
-                var template01 = template.replace("{identity}", value.identity);
-                template01 = template01.replace("{index}", index);
-                template01 = template01.replace("{favicon}", value.favicon);
-                formattedBokmarkItem = template01.replace("{title}", value.title);
-                bookmarkItems += formattedBokmarkItem;
-            });
-        }
-        $(bookmarkItems).appendTo($("#sortable"));
-    });
+  // Send message to page from add-on
+	self.port.on('currentmenuitems', function (menuItems) {
+		var bookmarkItems = '';
+		if (menuItems.length === 0) {
+			bookmarkItems = '<li id="nobookmarkfound" class="bookmark-menu-item"><img class="bookmark-menu-item-image" src="chrome://mozapps/skin/places/defaultFavicon.png"/>No searchable bookmark found!</li>';
+		} else {
+			$.each(menuItems, function (index, value) {
+				var template = '<li id="{identity}_{index}" class="bookmark-menu-item"><img class="bookmark-menu-item-image" src="{favicon}"/>{title}</li>';
+				var template01 = template.replace('{identity}', value.identity);
+				template01 = template01.replace('{index}', index);
+				template01 = template01.replace('{favicon}', value.favicon);
+				var formattedBokmarkItem = template01.replace('{title}', value.title);
+				bookmarkItems += formattedBokmarkItem;
+			});
+		}
+		$(bookmarkItems).appendTo($('#sortable'));
+	});
 
-    //Send message to add-on from page
-    $(document.body).on("click", "#btnSave", function (evt) {
-        evt.preventDefault();
-        var result = $("#sortable").sortable('toArray');
-        self.port.emit("btnSave", result);
-        $('#saveModal').modal('show');
-    });
+	// Send message to add-on from page
+	$(document.body).on('click', '#btnSave', function (evt) {
+		evt.preventDefault();
+		var result = $('#sortable').sortable('toArray');
+		self.port.emit('btnSave', result);
+		$('#saveModal').modal('show');
+	});
 
-    $("ul, li").disableSelection();
-    $(".list-group a").click(function () {
-        $(".list-group-item.active").removeClass("active");
-        $(this).addClass("active");
-    });
+	$('ul, li').disableSelection();
+	$('.list-group a').click(function () {
+		$('.list-group-item.active').removeClass('active');
+		$(this).addClass('active');
+	});
 
-    $("#sortable").sortable({
-        revert: true
-    });
+	$('#sortable').sortable({
+		revert: true
+	});
 
-    // to-do: use this for adding separators
-    // $( "#draggable" ).draggable({
-    //   connectToSortable: "#sortable",
-    //   helper: "clone",
-    //   revert: "invalid"
-    // });
+/* to-do: use this for adding separators
+$( "#draggable" ).draggable({
+connectToSortable: "#sortable",
+helper: "clone",
+revert: "invalid"
+});
+*/
 
-    if (location.hash.length > 0)
-        $("#menu a[href='" + location.hash + "']").tab("show");
+	if (location.hash.length > 0) {
+		$('#menu a[href=\'' + location.hash + '\']').tab('show');
+	}
 
-    self.port.on("addonversion", function (version) {
-        $("#about-extension-version").text(version);
-    });
+	self.port.on('addonversion', function (version) {
+		$('#about-extension-version').text(version);
+	});
 
-    loadChangelog();
+	loadChangelog();
 });
 
-function loadChangelog() {
-    $.ajax({
-        url: "../changelog.md",
-        dataType: "text",
-        success: function (text) {
-            var converter = new showdown.Converter();
-            var html = converter.makeHtml(text);
-            $("#changelog-content").html(html);
-        }
-    });
+function loadChangelog () {
+	$.ajax({
+		url: '../changelog.md',
+		dataType: 'text',
+		success: function (text) {
+			var converter = new showdown.Converter();
+			var html = converter.makeHtml(text);
+			$('#changelog-content').html(html);
+		}
+	});
 }
